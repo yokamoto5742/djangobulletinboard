861923e17fcc5cd8e79c24b5b15701e905c4db42
yokamoto5742-h <yokamoto5742@gmail.com>
2024-03-24 08:24:06 +0900

    1st commit

--- hello/views.py
+++ hello/views.py
@@ -1,24 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Article
 
 from .models import Greeting
 
 
 def index(request):
-    return render(request, "hello/index.html")
+    articles = Article.objects.all()
+    context = {
+        'articles': articles
+    }
+    return render(request, 'hello/index.html', context)
 
 
 def db(request):
-    # If you encounter errors visiting the `/db/` page on the example app, check that:
-    #
-    # When running the app on Heroku:
-    #   1. You have added the Postgres database to your app.
-    #   2. You have uncommented the `psycopg` dependency in `requirements.txt`, and the `release`
-    #      process entry in `Procfile`, git committed your changes and re-deployed the app.
-    #
-    # When running the app locally:
-    #   1. You have run `./manage.py migrate` to create the `hello_greeting` database table.
-
     greeting = Greeting()
     greeting.save()
 
