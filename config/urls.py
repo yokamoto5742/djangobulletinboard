8022db2d78bea5d72607763c8afd2b0b3d025ec7
yokamoto5742-h <yokamoto5742@gmail.com>
2024-03-24 15:19:38 +0900

    1st commit

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
-
-import hello.views
 
 urlpatterns = [
-    path("bbs/", include("bbs.urls")),
-    path("admin/", admin.site.urls),
-    path("hello/", include("hello.urls")),
+    path('bbs/', include('bbs.urls')),
+    path('hello/', include('hello.urls')),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', RedirectView.as_view(url='/bbs/')),
 ]
