8a524832d0c8ab3a2afb550e1bdd4a3f97807765
yokamoto5742-h <yokamoto5742@gmail.com>
2024-03-29 19:48:42 +0900

    1st commit

--- bbs/urls.py
+++ bbs/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path
 from . import views
 
-
 app_name = 'bbs'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('create/', views.CreateView.as_view(), name='create'),
-    path('<int:pk>/update/', views.UpdateView.as_view(), name='update'),
-    path('<int:pk>/delete/', views.DeleteView.as_view(), name='delete'),
+    path('update/<int:pk>/', views.UpdateView.as_view(), name='update'),
+    path('delete/<int:pk>/', views.DeleteView.as_view(), name='delete'),
     path('search/', views.search, name='search'),
 ]
